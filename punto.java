5600169968b6424966e2d9f450c384f6aae72bf2
Valen <104401300+ValenVillamarin@users.noreply.github.com>
2024-04-19 20:02:44 -0500

    Create punto.java

---
+++ punto.java
@@ -0,0 +1,11 @@
+public class punto {
+    private int x, y;
+    public double modulo() {
+        double valor = 0;
+        return valor;
+    }
+    public double fase () {
+        double valor = 0;
+        return valor;
+    } 
+}
